9d0a78c429bd69080cb92bbc2690024d00adf9f6
bptstbn <bony.baptiste@orange.fr>
2022-03-06 09:46:31 +0100

    MongoDB

--- server/mongo.js
+++ server/mongo.js
@@ -19,10 +19,14 @@ async function main()
     const products = loadjson();
     console.log(products);
 
-    // const collection = db.collection('products');
-    // const result = collection.insertMany(products);
-    
-    // console.log(result);
+
+    const collection = db.collection('products');
+    const result = collection.insertMany(products);
+    console.log(result);
+
+    const collectionsList = await client.db().listCollections().toArray();
+    const collectionNames = collectionsList.map(c => c.name);
+    console.log(collectionNames)
 
     process.exit(1);
 }
@@ -50,4 +54,36 @@ function loadjson()
     return products;
 }
 
-main()
+async function display()
+{
+    const client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
+    const db =  client.db(MONGODB_DB_NAME);
+    const collection = db.collection('products');
+
+    // find all products
+    // const products = await collection.find().toArray();
+    
+    
+
+    // find all products related to a given brand
+    const brand = 'adresse';
+    // const products = await collection.find({brand}).toArray();
+    
+    // find all products less than a price
+    var min = 0;
+    var max = 25;
+    const products = await collection.find({ 'price' : { $gt: min, $lt: max } }).toArray();
+    
+    // find all products sorted by price
+    var sort_price = { price: 1 };
+    //const products = await collection.find().sort(sort_price).toArray();
+
+    // display products
+    console.log(products);
+
+    process.exit(1);
+}
+
+
+display();
+
